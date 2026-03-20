0d6e1956c2c222c82f197f30441b17fb2f274791
CampbellWest <campbellwest1@gmail.com>
2026-03-20 10:32:21 -0400

    Added proposed route from documentation
    Routes dont do anything but are able to be viewed on swagger

---
+++ src/InventoryIntelligence/OrderController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+
+namespace InventoryIntelligence;
+
+[ApiController]
+[Route("api/inventory_intelligence/[controller]/[action]")]
+public class inventoryController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult all_items()
+    {
+        return Ok("Get everything in database");
+    }
+
+    // Not included but could imagine it being good for order team
+    [HttpGet]
+    public IActionResult get_items_by_type()
+    {
+        return Ok("Get everything in database ");
+    }
+
+    [HttpPost]
+    public IActionResult sold_items()
+    {
+        return Ok("Receive confirmed sold items");
+    }
+}
